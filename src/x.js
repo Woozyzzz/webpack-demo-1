6ec1e4049d505da6d2201aea055fa141a0dbd0a6
Woozyzzz <woozyzzz@foxmail.com>
2020-05-11 02:26:48 +0800

    抽离CSS

---
+++ src/x.js
@@ -0,0 +1,3 @@
+import "./x.css";
+console.log("我是x.js");
+export default "xxx";
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
@@ -6,5 +8,42 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "src/assets/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+        ],
+      },
+    ],
   },
 };
